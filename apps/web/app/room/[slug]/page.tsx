f2d4c298b02bef35514b78d8fa86ff78865d6f75
bhupesh <bhupeshverma29bv@gmail.com>
2025-01-30 03:19:33 +0530

    feat(db): initialize database schema and setup Prisma client

---
+++ apps/web/app/room/[slug]/page.tsx
@@ -0,0 +1,26 @@
+// import axios from "axios";
+// import { BACKEND_URL } from "../../config";
+
+// async function getRoomId(slug: string) {
+//     const response = await axios.get(`${BACKEND_URL}/room/${slug}`)
+//     console.log(response.data);
+//     return response.data.room.id;
+// }
+
+// eslint-disable-next-line react/display-name
+export default async function({
+    params
+}: {
+    params: {
+        slug: string
+    }
+}) {
+    // const slug = (await params).slug;
+    // const roomId = await getRoomId(slug);
+    
+    // return <ChatRoom id={roomId}></ChatRoom>
+    return <div>
+
+    </div>
+
+}
